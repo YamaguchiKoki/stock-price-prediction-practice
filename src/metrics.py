80c892e5807d3dcbc1a0ccc30c21c4ed39eeedf5
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-03-29 17:50:54 +0900

    fix validation metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, f1_score
 
 def regression_metrics(estimator, X, y):
     """回帰モデルの評価指標を計算する"""
@@ -7,4 +7,15 @@ def regression_metrics(estimator, X, y):
     return pd.DataFrame([{
         "RMSE": mean_squared_error(y, y_pred),
         "R2": r2_score(y, y_pred)
+    }])
+
+def classification_metrics(estimator, X, y, threshold=0.5):
+    y_pred_proba = estimator.predict(X)
+    y_pred = (y_pred_proba >= threshold).astype(int)
+
+    return pd.DataFrame([{
+        "Accuracy": accuracy_score(y, y_pred),
+        "Precision": precision_score(y, y_pred),
+        "Recall": recall_score(y, y_pred),
+        "F1": f1_score(y, y_pred)
     }])
